8723086ca2083478a0edfead9eff1017fe64dfb1
Samar Yadav <samarpratapjnv046@gmail.com>
2024-11-03 15:42:34 +0530

    fourth

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react';
 import Navbar from './Navbar'
 import { useParams } from 'react-router-dom'
 import { albumsData, assets, songsData } from '../assets/assets';
+import { PlayerContext } from '../context/PlayerContext';
 
 const DisplayAlbum = () => {
     const {id} = useParams();
     const albumData= albumsData[id];
+    const {playWithId} =useContext(PlayerContext);
   return (
     <>
       <Navbar/>
@@ -33,16 +35,18 @@ const DisplayAlbum = () => {
       <hr />
       {
         songsData.map((item,index)=>(
-            <div className='key={index} grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7 hover:bg-[#ffffff2b] cursor-pointer'>
+          <>
+            <div onClick={()=>playWithId(item.id)} className='key={index} grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7 hover:bg-[#ffffff2b] cursor-pointer'>
                 <p className='text-white'>
                     <b className=' mr-4 text-[#a7a7a7] '>{index+1}</b>
                     <img className='inline w-10 mr-5' src={item.image} alt="" />
                     {item.name}
                 </p>
-                <p className='text-[15px]  '>{albumData.name}</p>
+                <p className='text-[15px]'>{albumData.name}</p>
                 <p className='text-[15px] hidden sm:block ' >5 Days Ago</p>
                 <p className='text-[15px] text-center' >{item.duration}</p>
             </div>
+            </>
         ))
       }
     </>
--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useRef, useState } from "react";
+import React, { createContext, useEffect, useRef, useState } from "react";
 import { songsData } from "../assets/assets";
 export const PlayerContext = createContext();
 const PlayerContextProvider =(props)=>{
@@ -6,6 +6,8 @@ const PlayerContextProvider =(props)=>{
     const audioRef =useRef();
     const seekBg =useRef();
     const seekBar =useRef();
+
+
 
     const [track,setTrack] =useState(songsData[0]);
     const [playStatus,setPlayStatus] =useState(false);
@@ -29,6 +31,49 @@ const PlayerContextProvider =(props)=>{
         setPlayStatus(false);
     }
 
+    const playWithId = async(id)=>{
+        await setTrack(songsData[id]);
+        await audioRef.current.play();
+        setPlayStatus(true);
+    }
+
+    const previous = async ()=>{
+        if(track.id > 0){
+            await setTrack(songsData[track.id-1]);
+            await audioRef.current.play();
+            setPlayStatus(true);
+        }
+    }
+    const next = async ()=>{
+        if(track.id < songsData.length -1 ){
+            await setTrack(songsData[track.id+1]);
+            await audioRef.current.play();
+            setPlayStatus(true);
+        }
+    }
+
+    const seekSong =async (e)=>{
+        audioRef.current.currentTime =((e.nativeEvent.offsetX / seekBg.current.offsetWidth)*audioRef.current.duration)
+    }
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            audioRef.current.ontimeupdate = ()=>{
+                seekBar.current.style.width  = (Math.floor(audioRef.current.currentTime/audioRef.current.duration*100)) + "%";
+                setTime({
+                    currentTime:{
+                        second:Math.floor(audioRef.current.currentTime%60),
+                        minute:Math.floor(audioRef.current.currentTime/60)
+                    },
+                    totalTime:{
+                        second:Math.floor(audioRef.current.duration%60),
+                        minute:Math.floor(audioRef.current.duration/60)
+                    }
+                })
+            }
+        },1000)
+    },[audioRef]);
+
     const contextValue={
         audioRef,
         seekBar,
@@ -38,7 +83,10 @@ const PlayerContextProvider =(props)=>{
         playStatus,
         setPlayStatus,
         time,setTime,
-        play,pause 
+        play,pause,
+        playWithId,
+        previous,next,
+        seekSong
     }
 
     return (
