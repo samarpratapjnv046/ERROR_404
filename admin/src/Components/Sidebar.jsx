f4d5603fcc672455bceb97c422138fd5848ef446
Eklavya mohaniya <eklavyamohaniya@gmail.com>
2024-11-07 11:46:49 +0530

    admin panel

--- admin/src/Components/Sidebar.jsx
+++ admin/src/Components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { assets } from "../assets/assets"
+import { NavLink } from "react-router-dom"
 
 const Sidebar = () => {
   return(
@@ -7,12 +8,26 @@ const Sidebar = () => {
         <img src={assets.logo}  className= 'mt-5 w-[max(a0vw , 100px)] hidden sm:block' alt="" />
         <img src= {assets.logo_small} className='mt-5 w-[max(5vw,40px)] mr-5 sm:hidden block' alt="" />
             <div className='flex flex-col gap-5 mt-10'>
-                <div className='flex items-center gap-2.5 text-gray-800 bg-white border border-black p-2 pr-[max(8vw,10px)] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium'>
+               
+                <NavLink to= '/add-Song'  className='flex items-center gap-2.5 text-gray-800 bg-white border border-black p-2 pr-[max(8vw,10px)] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium'>
                     <img src={assets.add_song} className="w-5" alt="" />
                     <p className="hidden sm:block">Add Song</p>
+                </NavLink >
 
+                <NavLink to= '/list-song' className='flex items-center gap-2.5 text-gray-800 bg-white border border-black p-2 pr-[max(8vw,10px)] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium'>
+                    <img src={assets.song_icon} className="w-5" alt="" />
+                    <p className="hidden sm:block">List Song</p>
+                </NavLink>
 
-                </div>
+                <NavLink to= '/add-album' className='flex items-center gap-2.5 text-gray-800 bg-white border border-black p-2 pr-[max(8vw,10px)] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium'>
+                    <img src={assets.add_album} className="w-5" alt="" />
+                    <p className="hidden sm:block">Add Album</p>
+                </NavLink>
+
+                <NavLink to= '/list-album' className='flex items-center gap-2.5 text-gray-800 bg-white border border-black p-2 pr-[max(8vw,10px)] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium'>
+                    <img src={assets.album_icon} className="w-5" alt="" />
+                    <p className="hidden sm:block">List Album</p>
+                </NavLink>
 
             </div>
 
--- admin/src/pages/ListAlbum.jsx
+++ admin/src/pages/ListAlbum.jsx
@@ -4,8 +4,6 @@ const ListAlbum = () => {
   return(
     <div>
       list album
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla qui praesentium recusandae, est perspiciatis quam nostrum obcaecati pariatur. Repellat quae omnis consectetur deleniti illo iste, tempora labore voluptate quod fugit optio vitae aperiam numquam, facere aliquid voluptat
-      um distinctio delectus. Laudantium perspiciatis dicta quos! Quas quo ratione ipsa voluptate assumenda repellat.
     </div>
   )
 }
