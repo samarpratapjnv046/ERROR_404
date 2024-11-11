6b650ec9d2f4d08f544533bd153d45212e0aecd4
Samar Yadav <samarpratapjnv046@gmail.com>
2024-11-11 14:54:10 +0530

    sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ const Sidebar = () => {
 
   return (
     <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
-      <div className='bg-[#121212]  h-[15%] rounded flex flex-col justify-around'>
+      <div className='bg-[#ff8200]  h-[15%] rounded flex flex-col justify-around'>
         <div onClick={()=>navigate('/')} className='flex items-center gap-3 pl-8 cursor-pointer'>
             <img  className= 'w-6'src={assets.home_icon} alt=""/>
             <p className='font-bold '>Home</p>
@@ -18,7 +18,7 @@ const Sidebar = () => {
             <p className='font-bold '>Search</p>
         </div>
       </div>
-      <div className='bg-[#121212] h-[85%] rounded '>
+      <div className='bg-[#ff8200] h-[85%] rounded '>
         <div className='p-4 flex items-center justify-between'>
           <div className='flex items-center gap-3'>
             <img  className='w-8' src={assets.stack_icon} alt="" />
@@ -29,12 +29,12 @@ const Sidebar = () => {
             <img className='w-5' src={assets.plus_icon} alt='' />
           </div>
         </div>
-        <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
+        <div className='p-4 bg-[#4fe0f0] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
             <h1> Create Your First PlayList</h1>
             <p>It's easy we will help you </p>
             <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'> Create PlayList</button>
         </div>
-        <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
+        <div className='p-4 bg-[#4fe0f0] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
             <h1> Lets find some podcasts to follow</h1>
             <p>We will keep update on new episodes </p>
             <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'> Browse podcasts</button>
